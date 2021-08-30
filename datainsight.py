c8d59eb76bbfc081adb424106ab7089170722cfe
Franklin Adjei <57026567+Hotlynn2@users.noreply.github.com>
2021-08-30 17:04:07 +0000

    lower app jupyter notebook and script

---
+++ datainsight.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+txt = input("Enter your sentence: ")
+
+
+# In[ ]:
+
+
+txt = txt.split("_")
+
+
+# In[ ]:
+
+
+new_list = []
+for text in txt:
+    lower_txt = text.lower()
+    new_list.append(lower_txt)
+
+
+# In[ ]:
+
+
+new_sentence = ' '.join(new_list)
+
+
+# In[ ]:
+
+
+print(new_sentence)
+
+
+# In[ ]:
+
+
+
+
